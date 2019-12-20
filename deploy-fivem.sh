369b0a31a022e9c94cf1be1a2ae45e773c6bcc90
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-20 22:47:54 +0000

    updated quite a bit

--- deploy-fivem.sh
+++ deploy-fivem.sh
@@ -13,14 +13,33 @@ if [ $EUID != 0 ]; then
     sudo "$0" "$@"
     exit $?
 fi
-##############################
+###############################
 ###############################
 
+###############################
+# CONFIGURATION:
+# fill in appropriately
+#------------------------------
+#- Steam Key Goes Below:
+steamKey="D5E22AE91C9735328F8C397ACAA77389"
+
+#- FiveM License:
+cfx_license="iejska94yn650vfrh7dqy99ruuhxvw5m"
+
+#- MySql Account Info
+mysql_user="esmAdmin" #--> Essential Mode Database Admin
+mysql_password="vAELN964FMHvnHkuAJeXtzSZvZCvpD"
+global_mysql_user="Global_Admin" #--> This is for root login via PHPMYADMIN
+global_mysql_password="msspcXhl1iySar7rzRxMewH2iIPUVP23Yo4"
+
+#- Server Local Account
+srvAcct="fivem"
+srvPassword="lOI8cDDjwUC9O0VQhiihnudWT1VosP5Y4NvSNN"
 
 # Account Creation
 ########################
-
- adduser fivem
+    adduser $srvAcct --gecos "FiveM Server, , , " --disabled-password
+    echo "$srvAcct:$srvPassword" | chpasswd
 
 ########################
 
@@ -35,7 +54,7 @@ SOFTWARE=/var/software
         TCCORE=$TFIVEM/citizenfx.core.server
         TESMOD=$TFIVEM/essentialmode
 
-MAIN=/home/fivem
+MAIN=/home/$srvAcct
     GAME=$MAIN/server-data
         RESOURCES=$GAME/resources
             ESMOD=$RESOURCES/essentialmode
@@ -47,12 +66,6 @@ MAIN=/home/fivem
 
             SESX=$RESOURCES/\[sesx\]
 
-mysql_user="custodian"
-mysql_password="vAELN964FMHvnHkuAJeXtzSZvZCvpD"
-
-global_mysql_user="JayAdmin"
-global_mysql_password="msspcXhl1iySar7rzRxMewH2iIPUVP23Yo4"
-
 # TEMP DIRECTORIES
 mkdir $SOFTWARE
 mkdir $TFIVEM
@@ -63,14 +76,12 @@ mkdir $TESMOD
 # Dependancies
 ########################
 echo "Linux Software"
-# apt-get update
-# apt-get upgrade
-# apt-get -y install sudo
-# sudo apt-get -y install screen git
-# sudo apt-get -y install unzip unrar mariadb-server apache2 phpmyadmin
+ apt-get update
+ apt-get upgrade
+ sudo apt-get -y install unzip unrar-free mariadb-server apache2 phpmyadmin
 
 echo "mySQL"
-# mysql_secure_installation
+ mysql_secure_installation
 
 
 ## ---- mySQL Database ---- ##
@@ -80,6 +91,7 @@ echo "mySQL"
 
     mysql -e "CREATE USER '${global_mysql_user}'@'localhost' IDENTIFIED BY '${mysql_password}';"
     mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${global_mysql_user}'@'localhost';"
+## ---- mySQL Database ---- ##
 
 
 ## ---- FiveM ---- ##
@@ -103,7 +115,6 @@ echo "CitizenFX Module Update"
     cp -Rfup $TCCORE/CCORE/CitizenFX.Core.sym $MAIN/alpine/opt/cfx-server/citizen/clr2/lib/mono/4.5/CitizenFX.Core.sym
     cp -Rfup $TCCORE/CCORE/CitizenFX.Core.Server.dll $MAIN/alpine/opt/cfx-server/citizen/clr2/lib/mono/4.5/CitizenFX.Core.Server.dll
     cp -Rfup $TCCORE/CCORE/CitizenFX.Core.Server.sym $MAIN/alpine/opt/cfx-server/citizen/clr2/lib/mono/4.5/CitizenFX.Core.Server.sym
-
 ## ---- FiveM ---- ##
 
 
@@ -112,18 +123,7 @@ echo "CitizenFX Module Update"
 
 echo "sESX"
     wget -P "$TSESX" https://cdn.discordapp.com/attachments/529782752812204106/653783126253764649/sesx-current.zip
-    unzip $TSESX/sesx-current.zip -d $TSESX/source
-    mv -f "$TSESX/source/server-data/resources/'[essentialmode]'" "$TSESX/source/server-data/resources/[essentialmode]"
-    mv -f "$TSESX/source/server-data/resources/'[gamemodes]'" "$TSESX/source/server-data/resources/[gamemodes]"
-    mv -f "$TSESX/source/server-data/resources/[gamemodes]/'[maps]'" "$TSESX/source/server-data/resources/[gamemodes]/[maps]"
-    mv -f "$TSESX/source/server-data/resources/'[managers]'" "$TSESX/source/server-data/resources/[managers]"
-    mv -f "$TSESX/source/server-data/resources/'[test]'" "$TSESX/source/server-data/resources/[test]"
-    mv -f "$TSESX/source/server-data/resources/'[esx]'" "$TSESX/source/server-data/resources/[esx]"
-    mv -f "$TSESX/source/server-data/resources/'[gameplay]'" "$TSESX/source/server-data/resources/[gameplay]"
-    mv -f "$TSESX/source/server-data/resources/'[system]'" "$TSESX/source/server-data/resources/[system]"
-    mv -f "$TSESX/source/server-data/resources/[system]/'[builders]'" "$TSESX/source/server-data/resources/[system]/[builders]"
-    mv -f "$TSESX/source/server-data/resources/'[utility]'" "$TSESX/source/server-data/resources/[utility]"
-
+    $TSESX/sesx-current.zip -d $TSESX/source
 
     cp -Rfup $TSESX/source/server-data $MAIN
 
@@ -137,25 +137,47 @@ echo "sESX"
 
 ####### THIS SHOULD BE AT THE END STAGES ###########
 ## ---- Personalize the Configuration ---- ##
-mv /home/fivem/server-data/server.cfg{,.orig}
+mv /home/fivem/server-data/server.cfg{,.orig} #--> Renaming file to be processed
 
-Pass=`date +%s | sha256sum | base64 | head -c 64 ; echo`
-DateStamp=`date +"@%B#%Y"`
+#-RCON Password Creation
+echo "Generating RCON Password."
+    Pass=`date +%s | sha256sum | base64 | head -c 64 ; echo`
+    DateStamp=`date +"@%B#%Y"`
+    rcon_password="$Pass$DateStamp"
+    echo "RCON: $rcon_password"
+    echo ""
+    rcon_placeholder="#rcon_password changeme"
+    rcon_actual="rcon_password \"$rcon_password\""
+echo "Accepting original configuration; Injecting RCON configuration..."
+    sed "s/$rcon_placeholder/$rcon_actual/" $GAME/server.cfg.orig > $GAME/server.cfg.rconCfg
+    rm -f $GAME/server.cfg.orig  #--> cleaning up; handing off a .rconCfg
 
-db_conn="set mysql_connection_string \"server=localhost;database=essentialmode;userid=$mysql_user;password=$mysql_password\""
-steamKey="D5E22AE91C9735328F8C397ACAA77389"
-license="iejska94yn650vfrh7dqy99ruuhxvw5m"
+#-mySql Configuration
+echo "Accepting RCON config handoff; Injecting MySQL Connection String..."
+    db_conn_placeholder="set mysql_connection_string \"server=localhost;database=sesx;userid=username;password=YourPassword\""
+    db_conn_actual="set mysql_connection_string \"server=localhost;database=essentialmode;userid=$mysql_user;password=$mysql_password\""
+    sed "s/$db_conn_placeholder/$db_conn_actual/" $GAME/server.cfg.rconCfg > $GAME/server.cfg.dbCfg
+    rm -f $GAME/server.cfg.rconCfg #--> cleaning up; handing off a .dbCfg
 
-sed "s/#rcon_password changeme/rcon_password \"${Pass}${DateStamp}\"/" $GAME/server.cfg.orig > $GAME/server.cfg.rcon
-rm -f $GAME/server.cfg.orig
-sed "s/set mysql_connection_string \"server=localhost;database=sesx;userid=username;password=YourPassword\"/${db_conn}/" $GAME/server.cfg.rcon > $GAME/server.cfg.db
-rm -f $GAME/server.cfg.rcon
-sed "s/set steam_webApiKey \"SteamKeyGoesHere\"/steam_webApiKey  \"$steamKey\"/" $GAME/server.cfg.db > $GAME/server.cfg.steam
-rm -f $GAME/server.cfg.db
-sed "s/sv_licenseKey LicenseKeyGoesHere/sv_licenseKey $license/" $GAME/server.cfg.steam > $GAME/server.cfg
-rm -f $GAME/server.cfg.steam
+#-Steam Key Injection into Config
+echo "Accepted MySql config handoff; Injecting Steam Key into config..."
+    steamKey_placeholder="set steam_webApiKey \"SteamKeyGoesHere\""
+    steamKey_actual="steam_webApiKey  \"$steamKey\""
+    sed "s/$steamKey_placeholder/$steamKey_actual/" $GAME/server.cfg.dbCfg > $GAME/server.cfg.steamCfg
+    rm -f $GAME/server.cfg.dbCfg #--> cleaning up; handing off a .steamCfg
 
+#-FiveM License Key Injection into Config
+echo "Accepting Steam config handoff; Injecting FiveM License into config..."
+    sv_licenseKey_placeholder="sv_licenseKey LicenseKeyGoesHere"
+    sv_licenseKey_actual="sv_licenseKey $sv_licenseKey"
+    sed "s/$sv_license_placeholder/$sv_license_actual/" $GAME/server.cfg.steamCfg > $GAME/server.cfg
+    rm -f $GAME/server.cfg.steamCfg #--> cleaning up; handing off a server.cfg
 
+if [ -f $GAME/server.cfg ]; then
+    echo "Server configuration file found."
+else
+    echo "ERROR: Something went wrong during the configuration personalization..."
+fi
 
 #### THIS NEEDS TO BE LAST
-chown -R fivem:fivem $MAIN
+chown -R $srvAcct:$srvAcct $MAIN
